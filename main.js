38b73f9f8c4aba19bbcd116c09ecbc6e2c26da83
Beshalaby <beshalaby@gmail.com>
2022-10-05 23:31:20 -0400

    SQUARES!!!

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x24252A);
 //perspective camera mimics eyeball
 //(fov, aspect ratio, view frustum) 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000)
+const camera = new THREE.PerspectiveCamera(58, window.innerWidth/window.innerHeight, 0.1, 1000)
 
 //tell renderer what element to render in
 const renderer = new THREE.WebGL1Renderer({
@@ -31,9 +31,46 @@ scene.add(ambientLight);
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enabled = true;
 const gridHelper = new THREE.GridHelper(200, 50);
-scene.add(gridHelper);
+//scene.add(gridHelper);
 
 
+function get_random (list) {
+  return list[Math.floor((Math.random()*list.length))];
+}
+
+const cubeGeo = new THREE.BoxGeometry(5,5,5,10,10,10);
+var cubeMat = new THREE.MeshStandardMaterial({color: get_random([0x4242BE, 0x0066DB, 0x0081DF, 0x0096CE, 0x00A7B1, 0x00B68F])});
+
+
+
+var i = -40;
+
+while (i<50) {
+  
+  const cube1 = new THREE.Mesh(cubeGeo, cubeMat);
+  cube1.position.set(i,22,0);
+  scene.add(cube1);
+  var cubeMat = new THREE.MeshStandardMaterial({color: get_random([0x4242BE, 0x0066DB, 0x0081DF, 0x0096CE, 0x00A7B1, 0x00B68F])});
+  const cube2 = new THREE.Mesh(cubeGeo, cubeMat);
+  cube2.position.set(i,12,0);
+  scene.add(cube2);
+  var cubeMat = new THREE.MeshStandardMaterial({color: get_random([0x4242BE, 0x0066DB, 0x0081DF, 0x0096CE, 0x00A7B1, 0x00B68F])});
+  const cube3 = new THREE.Mesh(cubeGeo, cubeMat);
+  cube3.position.set(i,2,0);
+  scene.add(cube3);
+  var cubeMat = new THREE.MeshStandardMaterial({color: get_random([0x4242BE, 0x0066DB, 0x0081DF, 0x0096CE, 0x00A7B1, 0x00B68F])});
+  const cube4 = new THREE.Mesh(cubeGeo, cubeMat);
+  cube4.position.set(i,-8,0);
+  scene.add(cube4);
+  var cubeMat = new THREE.MeshStandardMaterial({color: get_random([0x4242BE, 0x0066DB, 0x0081DF, 0x0096CE, 0x00A7B1, 0x00B68F])});
+  const cube5 = new THREE.Mesh(cubeGeo, cubeMat);
+  cube5.position.set(i,-18,0);
+  scene.add(cube5);
+  // const cube6 = new THREE.Mesh(cubeGeo, cubeMat);
+  // cube6.position.set(i,0,0);
+  // scene.add(cube6);
+  i += 10;
+}
 
 
 
